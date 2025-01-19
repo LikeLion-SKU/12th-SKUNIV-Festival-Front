68877a25d68ff9272a99a09a1688c6a95039a997
changhoyu <tyu9817@naver.com>
2025-01-20 03:21:25 +0900

    🔧 Settings: alias 정의

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,16 @@ export default defineConfig({
   server: {
     port: 5173 
   },
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@routes', replacement: '/src/routes' },
+      { find: '@styles', replacement: '/src/styles' },
+      { find: '@utils', replacement: '/src/utils' },
+      { find: '@store', replacement: '/src/store' },
+    ]
+  }
 });
